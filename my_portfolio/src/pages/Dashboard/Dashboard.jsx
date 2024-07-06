01d8ef640b8e2dd7430d0e3ad7792ea40b3d4265
DorothyBeatrizDone <dbdone@hotmail.com>
2024-07-05 21:23:59 -0400

    Include axios request to get list of projects. Call Project Upload and ProjectCard components

---
+++ my_portfolio/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import ProjectUpload from '../components/ProjectUpload';
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+
+const host = import.meta.env.VITE_SERVER_HOST;
+const PORT = import.meta.env.VITE_SERVER_PORT;
+const baseUrl = `http://${host}:${PORT}`;
+const projectsUrl = `${baseUrl}/profile`;
+
+//Gets a list (in the form of cards) of the user's projects.
+const Dashboard = () => {
+  const [projects, setProjects] = useState([]);
+
+  const fetchProjects = async () => {
+    try {
+      const response = await axios.get(`${projectsUrl}/projects`);
+      setProjects(response.data);
+    } catch (err) {
+      console.log('Error fetching projects:', err);
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  return (
+    <section className="projects-page">
+      <h1>Your Dashboard</h1>
+      <ProjectUpload onProjectCreate={fetchProjects} />
+
+      {projects.map((project) => (
+        <ProjectCard key={project.id} project={project} />
+      ))}
+    </section>
+  );
+};
+
+export default Dashboard;
