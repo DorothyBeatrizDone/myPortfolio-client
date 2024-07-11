7aae2e10d863bb2499a01d753ebd422d30b30dad
DorothyBeatrizDone <dbdone@hotmail.com>
2024-07-11 16:34:44 -0400

    finished the dynamic text fields for the project upload. still need to test, debug, and add styling for these parts

--- my_portfolio/src/components/DynamicForm/DynamicForm.jsx
+++ my_portfolio/src/components/DynamicForm/DynamicForm.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
 
 const DynamicForm = ({label, name, items, setItems, type}) => {
-  //add all the inputs into setItems
+  //add a new empty input field.
+  //copy the current item array and add an empty string to the copied array
   const handleAddField = () =>{
-    for (item in items){
-        setItems(item);
-    }
+        setItems(...items, '');
   }
+
+  //removes an input field at a specified index
+  const handleRemoveField = (index) =>{
+      //remove the item at the specified index.
+        const updatedItems = items.filter((_, i) =>
+          i !== index
+        )
+        setItems(updatedItems);
+      }
+
+  //updates the value of the list of items at the specified index
+  const handleChange = (index, value) =>{
+    const updatedItems = items.map((item, i) =>
+      (i === index)? value : item
+    )
+    setItems(updatedItems);
+  }
+  //updates the value of an input field at a specified index.
+
     return (
         <>
-        items.map((item) => (
+        {/* We have an array of values. Iterate over the array of values 
+        and dynanically add a new field 
+        
+        After clicking the add {label} button, the handleAddField function
+        creates an empty string to the end of the list of items
+        since this string is empty, we need to get the value (e.target.value)
+        and send that to handleChange.*/}
+        {items.map((item,index) => (
+          <div>
             <input
             type={type}
             name={`${name}_${i}`}
@@ -18,12 +44,13 @@ const DynamicForm = ({label, name, items, setItems, type}) => {
             onChange={(e) => handleChange(index, e.target.value)}
             required
           />
-        ))
+            <button onClick={() => handleRemoveField(index)}>Remove</button>
+          </div>
+        ))}
         <button type="button" onClick={handleAddField}>
         Add {label}
       </button>
       </>
   )
-}
-
+};
 export default DynamicForm;
--- my_portfolio/src/pages/ProjectUpload/ProjectUpload.jsx
+++ my_portfolio/src/pages/ProjectUpload/ProjectUpload.jsx
@@ -10,26 +10,15 @@ const SERVER_URL = `http://${host}:${PORT}`;
 
 
 const ProjectUpload = ({onProjectCreate}) => {
-
-
-  //TBC, I am going to work now on something else. 
-  //Things to fix: list of skills and list of subjects, adding them in the form of tags.
-  //Also, I think I need to pass the user_id here. How do I get that again?
-  //no, we don't pass the id, but the backend has req.user.id... remember to print req. where
-  //does req.user come in?
-
   const [skills, setSkills] = useState([]);
   const [tags, setTags] = useState([]);
   const [files, setFiles] = useState([]);
-  //despite it's confusing name in the DB, subject is actaully a list
-  //note that it is written in JSON format!!
   const [subjects, setSubjects] = useState([]);
   const [failedAuth, setFailedAuth] = useState(false);
   const [error, setError] = useState("");
 
   const handleFormSubmit = async(e) => {
     e.preventDefault();
-    const { postTitle, projectUrl, endTime, visibilitySettings, subject, skill, projectDescription } = e.target;
     const token = localStorage.getItem('JWTtoken');
     if (!token) {
       setFailedAuth(true);
@@ -38,14 +27,14 @@ const ProjectUpload = ({onProjectCreate}) => {
 
     //Collect the user input 
     const uploadedForm = {
-      title: e.target.postTitle.value,
+      title: e.target.projectTitle.value,
       project_url: e.target.projectUrl.value,
       end_time: e.target.endTime.value,
       visibility: e.target.visibilitySettings.value, 
-      skills: e.target.skills.value,
+      skills,
       subject: e.target.subject.value,
-      tags: e.target.tags.value,
-      files:e.target.files.value,
+      tags,
+      files,
       description: e.target.projectDescription.value,
     };
 
@@ -55,36 +44,43 @@ const ProjectUpload = ({onProjectCreate}) => {
             headers: {
               Authorization: `Bearer ${token}`
             }
-          }
-        );
+        });
         onProjectCreate();
         e.target.reset();
+        {/* Reset the state after form submission since .reset() does not reset the states!*/}
+        setSkills([]);
+        setTags([]);
+        setFiles([]);
+        setSubjects([]);
     } catch (error) {
         setError('Error creating a new project.');
     }
-    // Post to API (remember to use `withCredentials`)
+  }
 
   return (
-    <section className="create-post">
-      <form className="post-form" onSubmit={handleFormSubmit}>
-        <h3>Create New Post</h3>
-        <div className="post-form__fields">
+    {failedAuth}?(
+      <p>Please log in to create a project.</p>
+    ):
+    <section className="create-project">
+      <form className="project-form" onSubmit={handleFormSubmit}>
+        <h3>Create New Project</h3>
+        <div className="project-form__fields">
           {/*Title */}
-          <div className="post-form__field">
-            <label htmlFor="postTitle" className="post-form__label">
+          <div className="project-form__field">
+            <label htmlFor="projectTitle" className="project-form__label">
               Project Title
             </label>
             <input
               type="text"
-              name="postTitle"
-              id="postTitle"
+              name="projectTitle"
+              id="projectTitle"
               maxLength="75"
               required
             />
           </div>
           {/*URL*/}
-          <div className="post-form__field">
-            <label htmlFor="projectUrl" className="post-form__label">
+          <div className="project-form__field">
+            <label htmlFor="projectUrl" className="project-form__label">
                 Project URL
               </label>
             <input 
@@ -95,20 +91,20 @@ const ProjectUpload = ({onProjectCreate}) => {
               required/>
           </div>
           {/*Description*/}
-          <div className="post-form__field">
-            <label htmlFor="postDescription" className="post-form__label">
-              Post Description
+          <div className="project-form__field">
+            <label htmlFor="projectDescription" className="project-form__label">
+              Project Description
             </label>
             <textarea
               type="text"
-              name="postDescription"
-              id="postDescription"
+              name="projectDescription"
+              id="projectDescription"
               required
             />
           </div>
           {/*End time*/}
-          <div className="post-form__field">
-            <label htmlFor="endTime" className="post-form__label">
+          <div className="project-form__field">
+            <label htmlFor="endTime" className="project-form__label">
                 Project Finished Date
               </label>
             <input 
@@ -117,8 +113,8 @@ const ProjectUpload = ({onProjectCreate}) => {
               name = "endTime"/>
           </div>
           {/*Visibility settings*/}
-          <div className="post-form__field">
-            <label htmlFor="visibilitySettings" className="post-form__label">
+          <div className="project-form__field">
+            <label htmlFor="visibilitySettings" className="project-form__label">
                 Visibility settings
               </label>
             <select 
@@ -134,43 +130,42 @@ const ProjectUpload = ({onProjectCreate}) => {
           Call the DynamicForm component!
           */
           }
-          <div className="post-form__field">
-            <label htmlFor="subject" className="post-form__label">
+          <div className="project-form__field">
+            <label htmlFor="subject" className="project-form__label">
                 Subject
               </label>
-              <DyanmicForm label="Subject" name="subject" item={subjects} setItems={setSubjects} type="text"/>
+              <DynamicForm label="Subject" name="subject" items={subjects} setItems={setSubjects} type="text"/>
           </div>
 
           {/*Skills (has add more functionality)*/}
-          <div className="post-form__field">
-              <label htmlFor="skill" className="post-form__label">
+          <div className="project-form__field">
+              <label htmlFor="skill" className="project-form__label">
                 Add Skill
               </label>
-              <DyanmicForm label="Skill" name="skill" item={skills} setItems={setSkills} type="text"/>
+              <DynamicForm label="Skill" name="skill" items={skills} setItems={setSkills} type="text"/>
             </div>
             {/*Tags (has add more functionality)*/}
-            <div className="post-form__field">
-              <label htmlFor="tag" className="post-form__label">
+            <div className="project-form__field">
+              <label htmlFor="tag" className="project-form__label">
                 Add Tag
               </label>
-              <DyanmicForm label="Tag" name="tag" item={tags} setItems={setTags} type="text"/>
+              <DynamicForm label="Tag" name="tag" items={tags} setItems={setTags} type="text"/>
             </div>
             {/*Files (has add more functionality)*/}
-            <div className="post-form__field">
-              <label htmlFor="file" className="post-form__label">
+            <div className="project-form__field">
+              <label htmlFor="file" className="project-form__label">
                 Add File URL
               </label>
-              <DyanmicForm label="File" name="file" item={files} setItems={setFiles} type="url"/>
+              <DynamicForm label="File" name="file" items={files} setItems={setFiles} type="url"/>
             </div>
         </div>
 
-        <button type="submit" className="post-form__submit">
+        <button type="submit" className="project-form__submit">
           Submit
         </button>
       </form>
     </section>
   );
-    };
 };
 
 export default ProjectUpload;
